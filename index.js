5840e09e0554762dff21b47d1cc5749b0e5bcfde
Minhajul-alam <alam.minhajul@outlook.com>
2023-11-07 17:09:31 +0600

    Get blog detail with id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,6 +27,13 @@ async function run() {
             const cursor = blogCollection.find();
             const result = await cursor.toArray();
             res.send(result)
+        })
+
+        app.get('/blogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await blogCollection.findOne(query);
+            res.send(result);
         })
 
         app.post('/blogs', async (req, res) => {
