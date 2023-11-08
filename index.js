89d61f6979fbca0fd541039b0e5c00091cd94f5b
Minhajul-alam <alam.minhajul@outlook.com>
2023-11-08 16:46:24 +0600

    Set delete item form wishlist

--- index.js
+++ index.js
@@ -109,6 +109,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/wishlist/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await wishlistCollection.deleteOne(query);
+            res.send(result);
+        })
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
     }
