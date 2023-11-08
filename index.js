8984675d1fec80df3133eb5c108c652f4f668e4d
Minhajul-alam <alam.minhajul@outlook.com>
2023-11-08 23:09:16 +0600

    Update vercel problems

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
@@ -107,8 +107,12 @@ async function run() {
             res.send(result);
         })
 
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
 }
 run().catch(console.dir);
